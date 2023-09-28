199065727ee89288814bfd3ca3e2f36365a129f8
LucasMaurojorge <lucas.maurojorge@anima.edu.uy>
2023-09-28 09:16:21 -0300

    todo

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import { Users } from "./components/users/Users.jsx";
+import { CountryUsers } from "./components/countryUsers/CountryUsers.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/usuarios",
+    element: <Users />,
+  },
+  {
+    path: "/usuarios/:pais",
+    element: <CountryUsers />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
